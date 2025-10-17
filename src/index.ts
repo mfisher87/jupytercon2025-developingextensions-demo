1fef976e9015a139558d5db6707d56ac6dae0a1b
Matt Fisher <3608264+mfisher87@users.noreply.github.com>
2025-10-17 17:06:11 -0600

    Add a "random image" widget and server component

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { MainAreaWidget, ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { imageIcon } from '@jupyterlab/ui-components';
 
 import { requestAPI } from './handler';
+import { ImageCaptionWidget } from './widget';
 
 /**
  * Initialization data for the myextension extension.
@@ -12,7 +16,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'myextension:plugin',
   description: 'A JupyterLab extension.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette, ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    // Dependencies are passed in in the same order they're defined in
+    // "requires" above:
+    palette: ICommandPalette,
+    launcher: ILauncher
+  ) => {
     console.log('JupyterLab extension myextension is activated!');
 
     requestAPI<any>('hello')
@@ -24,6 +35,27 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The myextension server extension appears to be missing.\n${reason}`
         );
       });
+
+    const command = 'image-caption:open';
+    app.commands.addCommand(command, {
+      execute: () => {
+        const widget = new ImageCaptionWidget();
+        const main = new MainAreaWidget({ content: widget });
+
+        // Set some metadata
+        main.title.label = 'Random image with caption';
+        main.title.caption = widget.title.label;
+        main.title.icon = imageIcon;
+
+        app.shell.add(main, 'main');
+      },
+      icon: imageIcon,
+      label: 'View a random image & caption'
+    });
+
+    // Add our command to the launcher and the command palette
+    launcher.add({ command });
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
